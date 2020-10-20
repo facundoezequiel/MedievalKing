5618c524fd01adc0f6cf11c9ae026ab02370a8ad
Facundo Brahim <facundo.e.brahim@gmail.com>
2020-10-20 19:54:10 -0300

    PowerUps Manager Fix

--- Assets/PowerUpsManager.cs
+++ Assets/PowerUpsManager.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpsManager : MonoBehaviour {
+    public Character character;
     public bool extra50Live = false;
     public bool extra3Force = false;
     public bool extra50Mana = false;
 
-    void Start () {
-
+    void Update () {
+        ActivarPowerUps ();
     }
 
-    void Update () {
-
+    public void ActivarPowerUps () {
+        // +50 de vida
+        if (extra50Live == true) {
+            character.stats.characterMaxLive = character.stats.characterMaxLive + 50;
+            if (character.stats.characterLive < character.stats.characterMaxLive) {
+                character.stats.characterLive = character.stats.characterMaxLive;
+            }
+            extra50Live = false;
+        }
+        // +3 de fuerza
+        if (extra3Force == true) {
+            character.stats.characterMinForce = character.stats.characterMinForce + 3;
+            character.stats.characterMaxForce = character.stats.characterMaxForce + 3;
+            character.stats.characterForce = character.stats.characterForce + 3;
+            extra3Force = false;
+        }
+        // +50 de mana 
+        if (extra50Mana == true) {
+            character.stats.characterMaxMana = character.stats.characterMaxMana + 50;
+            if (character.stats.characterMana < character.stats.characterMaxMana) {
+                character.stats.characterMana = character.stats.characterMaxMana;
+            }
+            extra50Mana = false;
+        }
     }
 }
