70cd76603869ccfd754975e3505fe2cb8bcfab6d
Facundo Brahim <facundo.e.brahim@gmail.com>
2020-11-11 23:16:39 -0300

    Final Boss Super Power

--- Assets/FinalBoss.cs
+++ Assets/FinalBoss.cs
@@ -16,7 +16,9 @@ public class FinalBoss : MonoBehaviour {
     public int finalBossMaxForce = 30;
     public int finalBossForce = 29;
     public int randomAttack;
+    public int superAttackRandom;
     public int randomHurt;
+    public bool finalBossSuperAttack = false;
     public bool floatingTextActive = false;
     public bool finalBossRecover = false;
     public bool finalBossAttackingZone = false;
@@ -25,6 +27,8 @@ public class FinalBoss : MonoBehaviour {
     public bool characterEnterInLeftZone = false;
     public FinalBossFollowRightZone finalBossFollowRightZone;
     public FinalBossFollowLeftZone finalBossFollowLeftZone;
+    public GameObject FinalBossParticlesRealodSuperPowerPrefab;
+    public GameObject FinalBossParticlesSuperPowerPrefab;
 
     void Start () {
         anim = GetComponent<Animator> ();
@@ -96,28 +100,32 @@ public class FinalBoss : MonoBehaviour {
     }
 
     public void FinalBossIdle () {
-        anim.SetBool ("isWalking", false);
-        anim.SetBool ("isHurt", false);
-        anim.SetBool ("isAttacking1", false);
-        anim.SetBool ("isAttacking2", false);
-        anim.SetBool ("isAttacking3", false);
-        anim.SetBool ("isBlock", false);
-        anim.SetBool ("isDying", false);
-        floatingTextActive = false;
-    }
-
-    public void FinalBossWalk () {
-        transform.position = new Vector3 (transform.position.x, positionY, transform.position.z);
-        if (finalBossRecover == false) {
-            anim.SetBool ("isWalking", true);
+        if (finalBossSuperAttack == false) {
+            anim.SetBool ("isWalking", false);
             anim.SetBool ("isHurt", false);
             anim.SetBool ("isAttacking1", false);
             anim.SetBool ("isAttacking2", false);
             anim.SetBool ("isAttacking3", false);
             anim.SetBool ("isBlock", false);
             anim.SetBool ("isDying", false);
-            transform.position = Vector2.MoveTowards (transform.position, target.position, finalBossMoveSpeed * Time.deltaTime);
             floatingTextActive = false;
+        }
+    }
+
+    public void FinalBossWalk () {
+        if (finalBossSuperAttack == false) {
+            transform.position = new Vector3 (transform.position.x, positionY, transform.position.z);
+            if (finalBossRecover == false) {
+                anim.SetBool ("isWalking", true);
+                anim.SetBool ("isHurt", false);
+                anim.SetBool ("isAttacking1", false);
+                anim.SetBool ("isAttacking2", false);
+                anim.SetBool ("isAttacking3", false);
+                anim.SetBool ("isBlock", false);
+                anim.SetBool ("isDying", false);
+                transform.position = Vector2.MoveTowards (transform.position, target.position, finalBossMoveSpeed * Time.deltaTime);
+                floatingTextActive = false;
+            }
         }
     }
 
@@ -129,77 +137,134 @@ public class FinalBoss : MonoBehaviour {
         anim.SetBool ("isAttacking1", false);
         anim.SetBool ("isAttacking2", false);
         anim.SetBool ("isAttacking3", false);
-        if (character.stats.characterDie == false && acurrenceAttack == 69) {
-            finalBossForce = Random.Range (finalBossMinForce, finalBossMaxForce);
-            character.stats.characterLive = character.stats.characterLive - finalBossForce;
-            character.actions.showLiveInText = finalBossForce;
-            character.actions.Hurt ();
-            anim.SetBool ("isWalking", false);
-            anim.SetBool ("isHurt", false);
+        if (character.stats.characterDie == false && acurrenceAttack == 69 && finalBossSuperAttack == false) {
             randomAttack = Random.Range (1, 4);
-            if (randomAttack == 1) {
-                anim.SetBool ("isAttacking1", true);
-            } else if (randomAttack == 2) {
-                anim.SetBool ("isAttacking2", true);
-            } else {
-                anim.SetBool ("isAttacking3", true);
+            if (randomAttack == 1 || randomAttack == 2) {
+                finalBossForce = Random.Range (finalBossMinForce, finalBossMaxForce);
+                character.stats.characterLive = character.stats.characterLive - finalBossForce;
+                character.actions.showLiveInText = finalBossForce;
+                character.actions.Hurt ();
+                anim.SetBool ("isWalking", false);
+                anim.SetBool ("isHurt", false);
+                anim.SetBool ("isBlock", false);
+                anim.SetBool ("isDying", false);
+                if (randomAttack == 1) {
+                    anim.SetBool ("isAttacking2", false);
+                    anim.SetBool ("isAttacking3", false);
+                    anim.SetBool ("isAttacking1", true);
+                } else if (randomAttack == 2) {
+                    anim.SetBool ("isAttacking1", false);
+                    anim.SetBool ("isAttacking3", false);
+                    anim.SetBool ("isAttacking2", true);
+                }
             }
-            anim.SetBool ("isBlock", false);
-            anim.SetBool ("isDying", false);
+            if (randomAttack == 3) {
+                superAttackRandom = Random.Range (0, 80);
+                if (superAttackRandom > 70) {
+                    finalBossSuperAttack = true;
+                    var FinalBossParticlesRealodSuperPower = Instantiate (FinalBossParticlesRealodSuperPowerPrefab, transform.position, Quaternion.identity, transform);
+                    anim.SetBool ("isWalking", false);
+                    anim.SetBool ("isHurt", false);
+                    anim.SetBool ("isAttacking1", false);
+                    anim.SetBool ("isAttacking2", false);
+                    anim.SetBool ("isAttacking3", false);
+                    anim.SetBool ("isBlock", true);
+                    anim.SetBool ("isDying", false);
+                    Invoke ("FinalBossSuperAttack", 2f);
+                } else {
+                    randomAttack = Random.Range (1, 3);
+                    finalBossForce = Random.Range (finalBossMinForce, finalBossMaxForce);
+                    character.stats.characterLive = character.stats.characterLive - finalBossForce;
+                    character.actions.showLiveInText = finalBossForce;
+                    character.actions.Hurt ();
+                    anim.SetBool ("isWalking", false);
+                    anim.SetBool ("isHurt", false);
+                    anim.SetBool ("isBlock", false);
+                    anim.SetBool ("isDying", false);
+                    if (randomAttack == 1) {
+                        anim.SetBool ("isAttacking2", false);
+                        anim.SetBool ("isAttacking3", false);
+                        anim.SetBool ("isAttacking1", true);
+                    } else if (randomAttack == 2) {
+                        anim.SetBool ("isAttacking1", false);
+                        anim.SetBool ("isAttacking3", false);
+                        anim.SetBool ("isAttacking2", true);
+                    }
+                }
+            }
         } else if (character.stats.characterDie == false && acurrenceAttack != 69) {
             anim.SetBool ("isWalking", false);
             anim.SetBool ("isHurt", false);
-            randomAttack = Random.Range (1, 4);
+            anim.SetBool ("isBlock", false);
+            anim.SetBool ("isDying", false);
+            randomAttack = Random.Range (1, 3);
             if (randomAttack == 1) {
                 anim.SetBool ("isAttacking1", true);
             } else if (randomAttack == 2) {
                 anim.SetBool ("isAttacking2", true);
-            } else {
-                anim.SetBool ("isAttacking3", true);
             }
-            anim.SetBool ("isBlock", false);
-            anim.SetBool ("isDying", false);
             character.actions.showLiveInText = 0;
         }
         floatingTextActive = false;
     }
 
-    // Function Block - Cuando bloquea un ataque
-    public void FinalBossBlock () {
+    // Function Super Attack - Cuando sale el super ataque en el random 
+    public void FinalBossSuperAttack () {
+        finalBossForce = Random.Range (finalBossMinForce, finalBossMaxForce);
+        character.stats.characterLive = character.stats.characterLive - (finalBossForce + 80);
+        character.actions.showLiveInText = finalBossForce;
+        character.actions.Hurt ();
+        var FinalBossParticlesSuperPower = Instantiate (FinalBossParticlesSuperPowerPrefab, transform.position, Quaternion.identity, transform);
         anim.SetBool ("isWalking", false);
-        anim.SetBool ("isHurt", true);
+        anim.SetBool ("isHurt", false);
+        anim.SetBool ("isBlock", false);
+        anim.SetBool ("isDying", false);
         anim.SetBool ("isAttacking1", false);
         anim.SetBool ("isAttacking2", false);
-        anim.SetBool ("isAttacking3", false);
-        anim.SetBool ("isBlock", true);
-        anim.SetBool ("isDying", false);
-        ShowFloatingText ();
+        anim.SetBool ("isAttacking3", true);
+        finalBossSuperAttack = false;
+    }
+
+    // Function Block - Cuando bloquea un ataque
+    public void FinalBossBlock () {
+        if (finalBossSuperAttack == false) {
+            anim.SetBool ("isWalking", false);
+            anim.SetBool ("isHurt", true);
+            anim.SetBool ("isAttacking1", false);
+            anim.SetBool ("isAttacking2", false);
+            anim.SetBool ("isAttacking3", false);
+            anim.SetBool ("isBlock", true);
+            anim.SetBool ("isDying", false);
+            ShowFloatingText ();
+        }
     }
 
     // Function Hurt - Cuando recibe un ataque
     public void FinalBossHurt () {
-        finalBossLive = finalBossLive - character.stats.characterForce;
-        if (character.stats.characterForce < character.stats.characterMaxForce - 1) {
-            anim.SetBool ("isWalking", false);
-            anim.SetBool ("isHurt", true);
-            anim.SetBool ("isAttacking1", false);
-            anim.SetBool ("isAttacking2", false);
-            anim.SetBool ("isAttacking3", false);
-            anim.SetBool ("isBlock", false);
-            anim.SetBool ("isDying", false);
-            finalBossRecover = false;
-        } else if (character.stats.characterForce == character.stats.characterMaxForce - 1) {
-            anim.SetBool ("isWalking", false);
-            anim.SetBool ("isHurt", true);
-            anim.SetBool ("isAttacking1", false);
-            anim.SetBool ("isAttacking2", false);
-            anim.SetBool ("isAttacking3", false);
-            anim.SetBool ("isBlock", false);
-            anim.SetBool ("isDying", false);
-            finalBossRecover = true;
-            Invoke ("FinalBossGettingUp", 0.7f);
+        if (finalBossSuperAttack == false) {
+            finalBossLive = finalBossLive - character.stats.characterForce;
+            if (character.stats.characterForce < character.stats.characterMaxForce - 1) {
+                anim.SetBool ("isWalking", false);
+                anim.SetBool ("isHurt", true);
+                anim.SetBool ("isAttacking1", false);
+                anim.SetBool ("isAttacking2", false);
+                anim.SetBool ("isAttacking3", false);
+                anim.SetBool ("isBlock", false);
+                anim.SetBool ("isDying", false);
+                finalBossRecover = false;
+            } else if (character.stats.characterForce == character.stats.characterMaxForce - 1) {
+                anim.SetBool ("isWalking", false);
+                anim.SetBool ("isHurt", true);
+                anim.SetBool ("isAttacking1", false);
+                anim.SetBool ("isAttacking2", false);
+                anim.SetBool ("isAttacking3", false);
+                anim.SetBool ("isBlock", false);
+                anim.SetBool ("isDying", false);
+                finalBossRecover = true;
+                Invoke ("FinalBossGettingUp", 0.7f);
+            }
+            ShowFloatingText ();
         }
-        ShowFloatingText ();
     }
 
     public void FinalBossGettingUp () {
